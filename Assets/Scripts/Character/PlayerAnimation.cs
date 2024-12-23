6d9b4a4d9731c8aa04da6311870f9d5416130550
Josh Klope <klope3@msn.com>
2024-12-23 09:38:03 -0600

    Add jump, fall, land animations

--- Assets/Scripts/Character/PlayerAnimation.cs
+++ Assets/Scripts/Character/PlayerAnimation.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ECM2;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField] private Character character;
     [SerializeField] private ECM2CharacterAdapter adapter;
     [SerializeField] private Animator animator;
 
     private int speedHash;
+    private int jumpHash;
+    private int landHash;
 
     private void Awake()
     {
-        speedHash = Animator.StringToHash("Speed");
+        speedHash = Hash("Speed");
+        jumpHash = Hash("Jump");
+        landHash = Hash("Land");
+
+        character.Jumped += Character_Jumped;
+        character.Landed += Character_Landed;
+    }
+
+    private void Character_Landed(Vector3 landingVelocity)
+    {
+        animator.SetTrigger(landHash);
+    }
+
+    private void Character_Jumped()
+    {
+        animator.SetTrigger(jumpHash);
     }
 
     private void Update()
     {
         animator.SetFloat(speedHash, adapter.GetMovementInput().magnitude);
+    }
+
+    private static int Hash(string str)
+    {
+        return Animator.StringToHash(str);
     }
 }
