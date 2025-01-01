ba5640c807c50cbc1126861563aff4de7dc00344
Josh Klope <klope3@msn.com>
2025-01-01 09:53:12 -0600

    Add basic player model

--- Assets/Scripts/Debug/TimeScale.cs
+++ Assets/Scripts/Debug/TimeScale.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class TimeScale : MonoBehaviour
 {
     [SerializeField] private float timeScale;
 
     private void Awake()
+    {
+        Time.timeScale = timeScale;
+    }
+
+    [Button]
+    public void SetTimeScale(float timeScale)
     {
         Time.timeScale = timeScale;
     }
