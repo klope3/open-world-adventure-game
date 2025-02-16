750b754cd38fdaa2246a814627f59d0dbfda1d77
Josh Klope <klope3@msn.com>
2025-02-16 09:25:17 -0600

    Add damage knockback

--- Assets/Scripts/Interaction/DamageZone.cs
+++ Assets/Scripts/Interaction/DamageZone.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ECM2;
 
 [RequireComponent(typeof(Collider))]
 public class DamageZone : MonoBehaviour
 {
     [SerializeField] private int amount;
+    [SerializeField] private float force;
+    [SerializeField] private Transform forceOrigin;
 
     private void OnTriggerStay(Collider other)
     {
@@ -14,6 +17,12 @@ public class DamageZone : MonoBehaviour
         {
             Vector3 point = other.ClosestPoint(transform.position);
             health.AddHealth(-1 * amount, point);
+        }
+
+        Character character = other.GetComponent<Character>();
+        if (character != null && force > 0)
+        {
+            character.AddExplosionForce(force, forceOrigin.position, 1000, 0, ForceMode.Impulse);
         }
     }
 }
