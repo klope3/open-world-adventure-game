736546fd949d545f232b7e5abcfd577301121ea4
Josh Klope <klope3@msn.com>
2025-02-19 16:52:43 -0600

    Add basic ambient sounds

---
+++ Assets/Scripts/World/Ambience/AmbienceManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("AudioSources assigned here will be randomly moved and played around the camera to simulate one-off ambient noises.")] 
+    private AudioSource[] oneOffPlayers;
+    
+    [SerializeField, Tooltip("Minimum seconds between one-off ambient noises.")] 
+    private float oneOffMinDelay;
+
+    [SerializeField, Tooltip("Maximum seconds between one-off ambient noises.")]
+    private float oneOffMaxDelay;
+
+    [SerializeField, Tooltip("Minimum distance from the camera to play one-off ambient noises.")]
+    private float oneOffMinDistance;
+
+    [SerializeField, Tooltip("Maximum distance from the camera to play one-off ambient noises.")]
+    private float oneOffMaxDistance;
+
+    private Camera mainCamera;
+    private float oneOffTimer;
+    private float oneOffTimerMax;
+
+    private void Awake()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        oneOffTimer += Time.deltaTime;
+        if (oneOffTimer > oneOffTimerMax)
+        {
+            PlayOneOff();
+            oneOffTimer = 0;
+            oneOffTimerMax = Random.Range(oneOffMinDelay, oneOffMaxDelay);
+        }
+    }
+
+    private void PlayOneOff()
+    {
+        if (oneOffPlayers == null || oneOffPlayers.Length == 0) return;
+
+        float randDistance = Random.Range(oneOffMinDistance, oneOffMaxDistance);
+        Vector3 randOffset = Random.insideUnitSphere.normalized * randDistance;
+        
+        int randIndex = Random.Range(0, oneOffPlayers.Length);
+        AudioSource randPlayer = oneOffPlayers[randIndex];
+
+        randPlayer.transform.position = mainCamera.transform.position += randOffset;
+        randPlayer.Play();
+    }
+}
