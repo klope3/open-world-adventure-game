dbb18665f1461c7a0df9337570de592b996ad92b
Josh Klope <klope3@msn.com>
2025-04-13 09:48:43 -0500

    Update attack animation events

--- Assets/Scripts/Character/CharacterAnimationEvents.cs
+++ Assets/Scripts/Character/CharacterAnimationEvents.cs
@@ -7,6 +7,7 @@ public class CharacterAnimationEvents : MonoBehaviour
 {
     public UnityEvent OnGenericAttack;
     public UnityEvent OnSwordSlashStart;
+    public UnityEvent OnSwordSlashEnd;
     public UnityEvent OnJumpStart;
     public UnityEvent OnRollStart;
     public UnityEvent OnDodgeStart;
@@ -20,6 +21,11 @@ public class CharacterAnimationEvents : MonoBehaviour
     public void Attack_SwordSlashStart()
     {
         OnSwordSlashStart?.Invoke();
+    }
+
+    public void Attack_SwordSlashEnd()
+    {
+        OnSwordSlashEnd?.Invoke();
     }
 
     public void Movement_JumpStart()
