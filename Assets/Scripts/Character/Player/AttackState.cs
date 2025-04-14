1ed5fafda5d86cf34cd1772bd1dad0558260537b
Josh Klope <klope3@msn.com>
2025-04-13 20:06:07 -0500

    Add debug indicator for movement input; make player snap to direction when attacking

--- Assets/Scripts/Character/Player/AttackState.cs
+++ Assets/Scripts/Character/Player/AttackState.cs
@@ -14,6 +14,14 @@ public class AttackState : PlayerState
     public override void EnterState()
     {
         characterAdapter.canMove = false;
+
+        Vector3 inputVec = InputActionsProvider.GetPrimaryAxis();
+        if (inputVec.magnitude > 0.05f)
+        {
+            inputVec = new Vector3(inputVec.x, 0, inputVec.y);
+            inputVec = inputVec.relativeTo(character.cameraTransform);
+            character.SetRotation(Quaternion.LookRotation(inputVec)); //ensure that player faces direction of input instantly; prevents getting stuck in an attack animation while still turning around
+        }
         //InputActionsProvider.OnBButtonStarted += Attack_started;
 
         OnEnter?.Invoke();
--- Assets/Scripts/Debug/CharacterDebugVisuals.cs
+++ Assets/Scripts/Debug/CharacterDebugVisuals.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using ECM2;
 using TMPro;
+using Sirenix.OdinInspector;
 
 public class CharacterDebugVisuals : MonoBehaviour
 {
@@ -10,10 +11,22 @@ public class CharacterDebugVisuals : MonoBehaviour
     [SerializeField] private GameObject groundedVisual;
     [SerializeField] private Character character;
     [SerializeField] private PlayerStateManager playerStateManager;
+    [SerializeField] private GameObject moveInputArrow;
 
     private void Update()
     {
         groundedVisual.SetActive(character.IsGrounded());
         playerStateText.text = playerStateManager.CurrentState.GetDebugName();
+        UpdateInputVector();
+    }
+
+    private void UpdateInputVector()
+    {
+        Vector3 startPoint = transform.position + Vector3.up;
+        Vector3 inputVec = InputActionsProvider.GetPrimaryAxis();
+        inputVec = new Vector3(inputVec.x, 0, inputVec.y);
+        inputVec = inputVec.relativeTo(character.cameraTransform);
+        moveInputArrow.transform.forward = inputVec.magnitude < 0.05f? Vector3.forward : inputVec;
+        moveInputArrow.transform.localScale = new Vector3(1, 1, inputVec.magnitude);
     }
 }
